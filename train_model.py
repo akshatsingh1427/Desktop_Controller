d53362440b22fb4222606e9f708c0a100eba6b4a
akshatsingh1427 <singhakshatamlesh@gmail.com>
2026-03-11 19:18:54 +0530

    Reformat train_model.py for consistency

--- train_model.py
+++ train_model.py
@@ -12,7 +12,7 @@ y = data.iloc[:, -1]    # Labels
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42)
 
-# Train model
+
 model = RandomForestClassifier(n_estimators=200)
 model.fit(X_train, y_train)
 
@@ -24,4 +24,5 @@ print("Model Accuracy:", accuracy)
 pickle.dump(model, open("model.pkl", "wb"))
 
 print("Model saved as model.pkl")
+
 
