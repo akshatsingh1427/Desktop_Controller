8eeae3ed8484efa6d0d05fd9ac3bb9652dcb28b5
akshatsingh1427 <singhakshatamlesh@gmail.com>
2026-03-14 08:05:34 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -16,7 +16,6 @@ X_train, X_test, y_train, y_test = train_test_split(
 model = RandomForestClassifier(n_estimators=200)
 model.fit(X_train, y_train)
 
-# Accuracy
 accuracy = model.score(X_test, y_test)
 print("Model Accuracy:", accuracy)
 
